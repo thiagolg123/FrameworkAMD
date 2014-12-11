c7ceea33ee6a7caa37cea8f16a3fe27363d280db
Thiago Goncalves <Thiago Goncalves@ADMINIB-4B8MMDS.br.ibm.com>
2014-12-11 16:55:10 -0200

    contribui na boa

---
+++ src/br/com/estudo/bean/Bean.java
@@ -0,0 +1,7 @@
+package br.com.estudo.bean;
+
+import java.io.Serializable;
+
+public interface Bean extends Serializable {
+
+}
--- src/br/com/estudo/bean/Usuario.java
+++ src/br/com/estudo/bean/Usuario.java
@@ -1,8 +1,12 @@
 package br.com.estudo.bean;
 
 
-public class Usuario {
+public class Usuario implements Bean {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String nome;
 	private String sobreNome;
 	
